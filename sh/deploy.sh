1b52af955885f0bacafa200c66ccf2b14f186edb
cluda <asgeir@cluda.no>
2016-01-05 15:55:21 +0100

    add certificat config for single environment

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -9,7 +9,8 @@ FOLDER=streams-app
 SERVICE_NAME=streams
 APPLICATION_NAME=TradersBit
 
-zip $CIRCLE_ARTIFACTS/$SERVICE_NAME Dockerfile Dockerrun.aws.json $SERVICE_NAME.jar .ebextensions/* .elasticbeanstalk/*
+mv .elasticbeanstalk_staging .elasticbeanstalk
+zip $CIRCLE_ARTIFACTS/$SERVICE_NAME Dockerfile Dockerrun.aws.json $SERVICE_NAME.jar .elasticbeanstalk/* .ebextensions/cwl-* .ebextensions/eb-*
 
 aws s3 cp $CIRCLE_ARTIFACTS/$SERVICE_NAME.zip s3://$EB_BUCKET/$FOLDER/$SERVICE_NAME-$SHA1.zip
 aws elasticbeanstalk create-application-version --application-name $APPLICATION_NAME --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$FOLDER/$SERVICE_NAME-$SHA1.zip
