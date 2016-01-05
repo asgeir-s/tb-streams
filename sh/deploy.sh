89073031b4935b05135304408883db3289456df8
cluda <asgeir@cluda.no>
2016-01-05 19:18:14 +0100

    Remove single instance https

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -10,7 +10,7 @@ SERVICE_NAME=streams
 APPLICATION_NAME=TradersBit
 
 mv .elasticbeanstalk_staging .elasticbeanstalk
-zip $CIRCLE_ARTIFACTS/$SERVICE_NAME Dockerfile Dockerrun.aws.json $SERVICE_NAME.jar .elasticbeanstalk/* .ebextensions/cwl-* .ebextensions/eb-*
+zip $CIRCLE_ARTIFACTS/$SERVICE_NAME Dockerfile Dockerrun.aws.json $SERVICE_NAME.jar .elasticbeanstalk/* .ebextensions/*
 
 aws s3 cp $CIRCLE_ARTIFACTS/$SERVICE_NAME.zip s3://$EB_BUCKET/$FOLDER/$SERVICE_NAME-$SHA1.zip
 aws elasticbeanstalk create-application-version --application-name $APPLICATION_NAME --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$FOLDER/$SERVICE_NAME-$SHA1.zip
